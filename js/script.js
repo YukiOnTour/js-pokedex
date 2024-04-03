9552b7b5ad42ec171ec68a8a3694ce267290a2c8
YukiOnTour <yukiontour@gmail.com>
2024-04-03 12:36:04 +0200

    Uploaded first files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Create a new array called pokemonList
+let pokemonList = [];
+
+// Add Pokémon objects to the array
+pokemonList.push({
+  name: "Bulbasaur",
+  height: 7,
+  types: ['grass', 'poison']
+});
+
+pokemonList.push({
+  name: "Charmander",
+  height: 6,
+  types: ['fire']
+});
+
+pokemonList.push({
+  name: "Squirtle",
+  height: 5,
+});
